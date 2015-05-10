4b8235857ba89c7b753c70c75eb615444820e0a7
Seth Reece <sethorama@bassplayer.st>
2015-05-10 09:36:45 -0700

    Added tests and validations for creating new contacts

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,2 +1,18 @@
 class Contact < ActiveRecord::Base
+
+	validates :first_name, presence: true
+	validates :last_name, presence: true,
+											  length: { minimum: 2 }
+	validates :street, presence: true,
+										 length: { minimum: 4 }
+	validates :city, presence: true,
+									 length: { minimum: 2 }
+	validates :state, presence: true,
+									  length: { minimum: 2 }
+	validates :zip_code, presence: true,
+											 length: { is: 5 },
+											 format: {
+											 	with: /[0-9]/
+											 }
+
 end
---
+++ spec/features/contacts/create_spec.rb
@@ -0,0 +1,137 @@
+require 'spec_helper'
+
+describe "Creating contacts" do
+
+	def create_contact(options={})
+		options[:first_name] ||= "First"
+		options[:last_name] ||= "Last"
+		options[:street] ||= "Street"
+		options[:city] ||= "City"
+		options[:state] ||= "ST"
+		options[:zip_code] ||= "11111"
+		options[:phone_number] ||= "5555551212"
+
+		visit "/contacts"
+		click_link "New Contact"
+
+		fill_in "First name", with: options[:first_name]
+		fill_in "Last name", with: options[:last_name]
+		fill_in "Street", with: options[:street]
+		fill_in "City", with: options[:city]
+		fill_in "State", with: options[:state]
+		fill_in "Zip code", with: options[:zip_code]
+		fill_in "Phone number", with: options[:phone_number]
+		click_button "Create Contact"
+	end
+
+	it "redirects to contact index page on success" do
+		create_contact
+		expect(page).to have_content("Contact was successfully created.")
+	end
+
+	it "gives an error with no first name" do
+		expect(Contact.count).to eq(0)
+
+		create_contact first_name: ""
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with no last name" do
+		expect(Contact.count).to eq(0)
+
+		create_contact last_name: ""
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with too short a last name" do
+		expect(Contact.count).to eq(0)
+
+		create_contact last_name: "L"
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with no street" do
+		expect(Contact.count).to eq(0)
+
+		create_contact street: ""
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with too short a street" do
+		expect(Contact.count).to eq(0)
+
+		create_contact street: "123"
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with no city" do
+		expect(Contact.count).to eq(0)
+
+		create_contact city: ""
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with too short a city" do
+		expect(Contact.count).to eq(0)
+
+		create_contact last_name: "C"
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with no state" do
+		expect(Contact.count).to eq(0)
+
+		create_contact state: ""
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with too short a state" do
+		expect(Contact.count).to eq(0)
+
+		create_contact state: "S"
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with no zip code" do
+		expect(Contact.count).to eq(0)
+
+		create_contact zip_code: ""
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with a zip code less than five digits" do
+		expect(Contact.count).to eq(0)
+
+		create_contact zip_code: "1234"
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with a zip code more than five digits" do
+		expect(Contact.count).to eq(0)
+
+		create_contact zip_code: "123456"
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+	it "gives an error with a zip code not numbers" do
+		expect(Contact.count).to eq(0)
+
+		create_contact zip_code: "abcde"
+		expect(page).to have_content("error")
+		expect(Contact.count).to eq(0)
+	end
+
+
+end
